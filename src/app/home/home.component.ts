60fa5701f30f8b2b0c28c4c26b7c1e70d162664b
unknown <afsanchezsa@unal.edu.co>
2021-01-27 15:20:34 -0500

    http part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,13 +16,14 @@ export class HomeComponent implements OnInit {
   dish:Dish;
   promotion:Promotion;
   leader:Leader;
+  dishErrMess:string;
   constructor(private dishService:DishService,
     private promotionService:PromotionService,
     private leaderService:LeaderService,
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
-    this.dishService.getFeaturedDish().subscribe(dish=>this.dish=dish);
+    this.dishService.getFeaturedDish().subscribe(dish=>this.dish=dish,errMess=>this.dishErrMess=errMess);
     this.promotionService.getFeaturedPromotion().subscribe(promo=>this.promotion=promo);
     this.leaderService.getFeaturedLeader().subscribe(leader=>this.leader=leader);
 
